c6cac785e0604417a86a8fb5aead465442811f56
Leo Brunette <leorbrunette@gmail.com>
2019-06-19 10:24:54 -0500

    Created Project

---
+++ ViewsAndControllers/ViewsAndControllers/Controllers/StringActivityController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewsAndControllers.Controllers
+{
+    public class StringActivityController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(viewName: "Index", model: "All starts here.");
+        }
+        [HttpPost]
+        public ActionResult SayHello(string inputString)
+        {
+            return View(viewName: "Index", model: "Hello " + inputString);
+        }
+    }
+}
---
+++ ViewsAndControllers/ViewsAndControllers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ViewsAndControllers.MvcApplication" Language="C#" %>
